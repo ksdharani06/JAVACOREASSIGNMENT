e5f5e72bbd724e221fd3a08375c8d5f3a145cc11
Dharani_ks <129415442+ksdharani06@users.noreply.github.com>
2025-11-30 21:44:41 +0530

    Update Account.java

--- Question4-Banking/Account.java
+++ Question4-Banking/Account.java
@@ -1,12 +1,12 @@
 abstract class Account {
 
-   // String accountNumber;
+  
     String accountHolderName;
     double balance;
 
 
     public Account(String accountNumber, String accountHolderName, double balance) {
-      //  this.accountNumber = accountNumber;
+    
         this.accountHolderName = accountHolderName;
         this.balance = balance;
     }
