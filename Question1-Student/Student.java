869d0b010e265164f5fbf65185b37d3b0419b95d
ksDharani <ksdharani0197@gmail.com>
2025-11-30 21:37:33 +0530

    Question1 is commiting

---
+++ Question1-Student/Student.java
@@ -0,0 +1,40 @@
+public class Student {
+    private int studentId;
+    private String name;
+    private char grade;
+    private double marks;
+
+    public Student(int studentId, String name, char grade, double marks) {
+        this.studentId = studentId;
+        this.name = name;
+        this.grade = grade;
+        this.marks = marks;
+    }
+
+    // Getters
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+               "id=" + studentId +
+               ", name='" + name + '\'' +
+               ", grade=" + grade +
+               ", marks=" + marks +
+               '}';
+    }
+}
