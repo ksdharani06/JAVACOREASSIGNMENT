11296afaf08a3233f58c50543da320a36138bba4
Dharani_ks <129415442+ksdharani06@users.noreply.github.com>
2025-11-30 22:06:21 +0530

    Update StudentManagement.java

--- Question1-Student/StudentManagement.java
+++ Question1-Student/StudentManagement.java
@@ -1,71 +1,86 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StudentManagement {
 
     public static void main(String[] args) {
 
         ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student(1, "dharani", 'A', 92.5));
-        students.add(new Student(2, "shanthi", 'B', 78.0));
-        students.add(new Student(3, "Chitra", 'A', 88.0));
-        students.add(new Student(4, "Deepak", 'C', 55.5));
-        students.add(new Student(5, "Esha", 'D', 42.0));
-        students.add(new Student(6, "ram", 'F', 35.0));
-        students.add(new Student(7, "Gita", 'B', 69.0));
-        students.add(new Student(8, "dharu", 'A', 95.0));
-        students.add(new Student(9, "Indira", 'C', 60.0));
-        students.add(new Student(10, "guru", 'B', 81.0));
 
-        System.out.println("Students with grade 'A':");
+        students.add(new Student(1, "dharani", 'A', 92.5));
+        students.add(new Student(2, "shanthi", 'B', 78));
+        students.add(new Student(3, "Chitra", 'A', 88));
+        students.add(new Student(4, "Deepak", 'C', 55.5));
+        students.add(new Student(5, "Esha", 'D', 42));
+        students.add(new Student(6, "ram", 'F', 35));
+        students.add(new Student(7, "Gita", 'B', 69));
+        students.add(new Student(8, "dharu", 'A', 95));
+        students.add(new Student(9, "Indira", 'C', 60));
+        students.add(new Student(10, "guru", 'B', 81));
+
+        System.out.println("Students with grade A:");
         for (Student s : students) {
             if (s.getGrade() == 'A') {
                 System.out.println(s);
             }
         }
-        System.out.println();
 
-        students.removeIf(s -> s.getMarks() < 40);
+       
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getMarks() < 40) {
+                students.remove(i);
+                i--; 
+            }
+        }
 
         System.out.println("After removing students with marks < 40:");
         for (Student s : students) {
             System.out.println(s);
         }
-        System.out.println();
 
-    
-        Collections.sort(students, new Comparator<Student>() {
-            @Override
-            public int compare(Student s1, Student s2) {
-                return s1.getName().compareToIgnoreCase(s2.getName());
+        for (int i = 0; i < students.size(); i++) {
+            for (int j = i + 1; j < students.size(); j++) {
+                if (students.get(i).getName()
+                        .compareToIgnoreCase(students.get(j).getName()) > 0) {
+
+                    Student temp = students.get(i);
+                    students.set(i, students.get(j));
+                    students.set(j, temp);
+                }
             }
-        });
+        }
 
-
+        System.out.println("Students sorted by name:");
         for (Student s : students) {
             System.out.println(s);
         }
-        System.out.println();
 
-
-        Map<Character, Integer> gradeCount = new HashMap<>();
-        gradeCount.put('A', 0);
-        gradeCount.put('B', 0);
-        gradeCount.put('C', 0);
-        gradeCount.put('D', 0);
-        gradeCount.put('F', 0);
+        int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
 
         for (Student s : students) {
-            char grade = s.getGrade();
-            gradeCount.put(grade, gradeCount.getOrDefault(grade, 0) + 1);
+            switch (s.getGrade()) {
+                case 'A':
+                    countA++;
+                    break;
+                case 'B':
+                    countB++;
+                    break;
+                case 'C':
+                    countC++;
+                    break;
+                case 'D':
+                    countD++;
+                    break;
+                case 'F':
+                    countF++;
+                    break;
+            }
         }
 
-        System.out.println("grade frq   : ");
-        for (Map.Entry<Character, Integer> entry : gradeCount.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
+        System.out.println("Grade Frequency:");
+        System.out.println("A -> " + countA);
+        System.out.println("B -> " + countB);
+        System.out.println("C -> " + countC);
+        System.out.println("D -> " + countD);
+        System.out.println("F -> " + countF);
     }
 }
