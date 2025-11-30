e8ce2259c5da0e8bae9d207bb0dfa4d7b9bbc3d1
Dharani_ks <129415442+ksdharani06@users.noreply.github.com>
2025-11-30 21:52:08 +0530

    Update TaskManager.java

--- Question3-PriorityQueue/TaskManager.java
+++ Question3-PriorityQueue/TaskManager.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class TaskManager {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         
         PriorityQueue<Task> queue = new PriorityQueue<>(
